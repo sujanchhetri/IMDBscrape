fcf209f8851ff1285aa9a799b279d23d6c95a39f
Sujan Chhetri <44917558+sujanchhetri@users.noreply.github.com>
2020-04-02 20:09:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 const Json2csvParser = require('json2csv').Parser;
 
 const URLS = [
-    'https://www.imdb.com/title/tt0102926/?ref_=nv_sr_1', 
-    'https://www.imdb.com/title/tt2267998/?ref_=nv_sr_1'
+    'https://www.imdb.com/title/tt0111161/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=e31d89dd-322d-4646-8962-327b42fe94b1&pf_rd_r=5MFAZZS1R7W4HT4MNKSZ&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_1', 
+    'https://www.imdb.com/title/tt0050083/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=e31d89dd-322d-4646-8962-327b42fe94b1&pf_rd_r=5MFAZZS1R7W4HT4MNKSZ&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_tt_5'
 ];
 
 (async () => {
